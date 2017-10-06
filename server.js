5f88a3bf71c81d4b3d8f373f7a1be48f305ef50f
Linus Metzler <hi@linusmetzler.me>
2017-10-06 17:13:53 +0200

    upgraded express validator

--- server.js
+++ server.js
@@ -2,50 +2,45 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 const { exec } = require('child_process');
 const express = require('express');
-const expressValidator = require('express-validator');
-const util = require('util');
+const { check, validationResult } = require('express-validator/check');
+const { matchedData } = require('express-validator/filter');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressValidator({
-    customValidators: {
-        gte(param, num) {
-            return param >= num;
-        },
-        isCode128(param) {
-            return /^[\x20-\x7F]*$/.test(param);
-        },
-        isDate(param) {
-            return !Number.isNaN(Date.parse(param));
-        },
-    },
-}));
 
 function ts() { return moment().format('YYYY-MM-DD HH:mm:ss'); }
 
-app.post('/', (req, res) => {
-    req.checkBody('title', 'title missing').notEmpty();
-    req.checkBody('barcode', 'barcode missing or invalid').notEmpty().isCode128();
-    req.checkBody('timestamp', 'timestamp missing or invalid').notEmpty().isDate();
-    req.checkBody('copies', 'copies missing or invalid').notEmpty().isInt().gte(1);
+app.post('/', [
+    check('title')
+        .not().isEmpty(),
+    check('barcode', 'Has to be a valid Code 128')
+        .not().isEmpty()
+        .custom(value => /^[\x20-\x7F]*$/.test(value)),
+    check('timestamp', 'Must be a parseable date')
+        .not().isEmpty()
+        .custom(value => !Number.isNaN(Date.parse(value))),
+    check('copies', 'At least 1')
+        .not().isEmpty()
+        .isInt({ min: 1 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.mapped() });
+    }
 
-    req.getValidationResult().then((result) => {
-        if (!result.isEmpty()) {
-            res.status(400).send(`There have been validation errors: ${util.inspect(result.array())}`);
-            return;
+    const data = matchedData(req);
+
+    return exec(`${path.join(__dirname, '/bpac-barcode/bpac-barcode.exe')} "${data.title}" "${data.barcode}" "${data.timestamp}" ${data.copies}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(ts(), data, 'Printing failed', { exitCode: error.code }, stderr);
+            res.status(500).send('Printing failed');
+        } else {
+            console.log(ts(), data, 'Printed');
+            res.status(200).send('Printed');
         }
-        exec(`${path.join(__dirname, '/bpac-barcode/bpac-barcode.exe')} "${req.body.title}" "${req.body.barcode}" "${req.body.timestamp}" ${req.body.copies}`, (error, stdout, stderr) => {
-            if (error) {
-                console.error(ts(), req.body, 'Printing failed', { exitCode: error.code }, stderr);
-                res.status(500).send('Printing failed');
-            } else {
-                console.log(ts(), req.body, 'Printed');
-                res.status(200).send('Printed');
-            }
-        });
     });
 });
 
